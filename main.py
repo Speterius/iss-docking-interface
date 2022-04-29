0e4fd2532adf7f616a8bbff8bbcea19d015fd195
pseres <peet.seres@gmail.com>
2022-04-29 11:04:19 +0200

    Added control interface

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.remote.webelement import WebElement
 
 
 class Elements:
-    """ The names of the corresponding html or css elements. """
+    """The names of the corresponding html or css elements."""
 
     roll = (By.ID, "roll")
     pitch = (By.ID, "pitch")
@@ -26,7 +26,7 @@ class Elements:
 
 
 class UnitParser:
-    """ Parse the retreived strings into floats. (ditch the units) """
+    """Parse the retreived strings into floats. (ditch the units)"""
 
     angle = "°"
     angular_rate = "°/s"
@@ -60,23 +60,37 @@ def get_rate(webelement: WebElement) -> float:
     return UnitParser.parse_rate(value)
 
 
-class WebElementInterface:
-    """ This class keeps an instance of each web element so that the user can fetch data from the browser. """
+class StateInterface:
+    """This class keeps an instance of each web element so that the user can fetch data from the browser."""
 
     def __init__(self, browser: WebDriver):
-        self.roll: WebElement = browser.find_element(*Elements.roll).find_element(*Elements.error)
-        self.pitch: WebElement = browser.find_element(*Elements.pitch).find_element(*Elements.error)
-        self.yaw: WebElement = browser.find_element(*Elements.yaw).find_element(*Elements.error)
+        self.roll: WebElement = browser.find_element(*Elements.roll).find_element(
+            *Elements.error
+        )
+        self.pitch: WebElement = browser.find_element(*Elements.pitch).find_element(
+            *Elements.error
+        )
+        self.yaw: WebElement = browser.find_element(*Elements.yaw).find_element(
+            *Elements.error
+        )
 
-        self.rollrate: WebElement = browser.find_element(*Elements.roll).find_element(*Elements.rate)
-        self.pitchrate: WebElement = browser.find_element(*Elements.pitch).find_element(*Elements.rate)
-        self.yawrate: WebElement = browser.find_element(*Elements.yaw).find_element(*Elements.rate)
+        self.rollrate: WebElement = browser.find_element(*Elements.roll).find_element(
+            *Elements.rate
+        )
+        self.pitchrate: WebElement = browser.find_element(*Elements.pitch).find_element(
+            *Elements.rate
+        )
+        self.yawrate: WebElement = browser.find_element(*Elements.yaw).find_element(
+            *Elements.rate
+        )
 
         self.pos_x = browser.find_element(*Elements.pos_x).find_element(*Elements.pos)
         self.pos_y = browser.find_element(*Elements.pos_y).find_element(*Elements.pos)
         self.pos_z = browser.find_element(*Elements.pos_z).find_element(*Elements.pos)
 
-        self.velocity = browser.find_element(*Elements.velocity_rate).find_element(*Elements.rate)
+        self.velocity = browser.find_element(*Elements.velocity_rate).find_element(
+            *Elements.rate
+        )
 
     def get_angles(self) -> (float, float, float):
         roll = UnitParser.parse_angle(self.roll.text)
@@ -96,36 +110,109 @@ class WebElementInterface:
         pos_z = UnitParser.parse_position(self.pos_z.text)
         return pos_x, pos_y, pos_z
 
-    def get_velocities(self) -> float:
+    def get_velocity(self) -> float:
         return UnitParser.parse_velocity(self.velocity.text)
+
+
+class ActionInterface:
+    def __init__(self, browser):
+        self.browser = browser
+
+        self.ctrl_pitch_up = browser.find_element(By.ID, "pitch-up-button")
+        self.ctrl_pitch_down = browser.find_element(By.ID, "pitch-down-button")
+        self.ctrl_roll_left = browser.find_element(By.ID, "roll-left-button")
+        self.ctrl_roll_right = browser.find_element(By.ID, "roll-right-button")
+        self.ctrl_yaw_left = browser.find_element(By.ID, "yaw-left-button")
+        self.ctrl_yaw_right = browser.find_element(By.ID, "yaw-right-button")
+        self.ctrl_translate_left = browser.find_element(By.ID, "translate-left-button")
+        self.ctrl_translate_right = browser.find_element(
+            By.ID, "translate-right-button"
+        )
+        self.ctrl_translate_up = browser.find_element(By.ID, "translate-up-button")
+        self.ctrl_translate_down = browser.find_element(By.ID, "translate-down-button")
+        self.ctrl_translate_forward = browser.find_element(
+            By.ID, "translate-forward-button"
+        )
+        self.ctrl_translate_backward = browser.find_element(
+            By.ID, "translate-backward-button"
+        )
+
+    def pitch_up(self):
+        self.ctrl_pitch_up.click()
+
+    def pitch_down(self):
+        self.ctrl_pitch_down.click()
+
+    def yaw_left(self):
+        self.ctrl_yaw_left.click()
+
+    def yaw_right(self):
+        self.ctrl_yaw_right.click()
+
+    def roll_left(self):
+        self.ctrl_roll_left.click()
+
+    def roll_right(self):
+        self.ctrl_roll_right.click()
+
+    def translate_left(self):
+        self.ctrl_translate_left.click()
+
+    def translate_right(self):
+        self.ctrl_translate_right.click()
+
+    def translate_up(self):
+        self.ctrl_translate_up.click()
+
+    def translate_down(self):
+        self.ctrl_translate_down.click()
+
+    def translate_forward(self):
+        self.ctrl_translate_forward.click()
+
+    def translate_backward(self):
+        self.ctrl_translate_backward.click()
+
+
+class DockingInterface:
+    def __init__(self, browser: WebDriver):
+        self.states = StateInterface(browser)
+        self.actions = ActionInterface(browser)
+
+
+class WebsiteInterface:
+    def __init__(self, browser: WebDriver):
+        self.begin_button = browser.find_element(By.ID, "begin-button")
 
 
 def main():
     # Set up the webdriver
     service = Service("./chromedriver.exe")
     options = ChromeOptions()
-    browser = Chrome(service=service, options=options)
-
     # options.headless = True
+    with Chrome(service=service, options=options) as browser:
 
-    # Go to the website
-    browser.get("http://localhost:5555/iss-sim.spacex.com")
+        # Go to the website
+        browser.get("http://localhost:5555/iss-sim.spacex.com")
 
-    # Web Element interface
-    w = WebElementInterface(browser)
+        # Web Element interface
+        w = DockingInterface(browser)
 
-    print(f"waiting for 10 seconds:")
-    time.sleep(15)
+        print(f"waiting for 15 seconds:")
+        time.sleep(15)
 
-    # Angles
-    print(w.get_angles())
+        print(f"Pringing states for 10 seconds as a test ... ")
+        for i in range(20):
+            print(f"\n_____________________")
+            print(f"Angles: {w.states.get_angles()}")
+            print(f"Agnular rates: {w.states.get_angular_rates()}")
+            print(f"Positions: {w.states.get_positions()}")
+            print(f"Velocity: {w.states.get_velocity()}")
 
-    # Angular rates
-    print(w.get_angular_rates())
+            time.sleep(1)
 
-    # Positions
-    print(w.get_positions())
+    print("Closing session")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
